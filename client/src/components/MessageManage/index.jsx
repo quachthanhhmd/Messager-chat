d7fe80daf23fc66c56b52ee77bec3a7d9dafc24d
quachthanhhmd <48387076+quachthanhhmd@users.noreply.github.com>
2021-09-29 23:17:48 +0700

    Add: List expandable Customize chat - client.

--- client/src/components/MessageManage/index.jsx
+++ client/src/components/MessageManage/index.jsx
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-
 import "./index.scss";
 
+import Collapsible from 'react-collapsible';
+
+
+
+
 MessageManage.propTypes = {
-    
+
 };
 
 function MessageManage(props) {
+
+    const [isActive, setIsActive] = useState(false);
+
     return (
         <div className="manage-main">
             <div className="manage-main___header">
                 <div className="manage-main___header--avatar" >
-                    <img src="/avatar.png" alt="avatar"/>
+                    <img src="/avatar.png" alt="avatar" />
                     <div className="manage-main___header--avatar___active-in">
                     </div>
                 </div>
@@ -24,6 +31,72 @@ function MessageManage(props) {
                 </div>
 
             </div>
+            <div className="manage-main___slide-list">
+
+                {/* <Collapsible classParentString="manage-main___slide-list___custom-chat" trigger="Customize Chat">
+                        <p>
+                            This is the collapsible content. It can be any element or React
+                            component you like.
+                        </p>
+                        <p>
+                            It can even be another Collapsible component. Check out the next
+                            section!
+                        </p>
+                    </Collapsible> */}
+                <div className="manage-main___slide-list___custom-chat">
+                    <div className="manage-main___slide-list--collapsible"
+                        onClick={() => { setIsActive(!isActive) }}>
+                        Customize Chat
+                    </div>
+                    <div className={`manage-main___slide-list--content ${isActive ? " manage-main___slide-list--show" : " manage-main___slide-list--hidden"}`}>
+                        <div className="manage-main___slide-list--content___item">
+                            Change Theme
+                        </div>
+                        <div className="manage-main___slide-list--content___item">
+                            Change Emoji
+                        </div>
+                        <div className="manage-main___slide-list--content___item">
+                            <div className="manage-main___slide-list--content___item--icon">
+                                <img src="/nick-name.png" alt="Edit nick name" />
+                            </div>
+                            <div className="manage-main___slide-list--content___item--name">
+                                Edit Nicknames
+                            </div>
+                
+                        </div>
+                        <div className="manage-main___slide-list--content___item">
+                            Search in Conversation
+                        </div>
+                    </div>
+                </div>
+                <div className="manage-main___slide-list___custom-chat">
+                    <div className="manage-main___slide-list--collapsible"
+                        onClick={() => { setIsActive(!isActive) }}>
+                        Customize Chat
+                    </div>
+                    <div className={`manage-main___slide-list--content ${isActive ? " manage-main___slide-list--show" : " manage-main___slide-list--hidden"}`}>
+                        <div className="manage-main___slide-list--content___item">
+                            Change Theme
+                        </div>
+                        <div className="manage-main___slide-list--content___item">
+                            Change Emoji
+                        </div>
+                        <div className="manage-main___slide-list--content___item">
+                            <div className="manage-main___slide-list--content___item--icon">
+                                <img src="/nick-name.png" alt="Edit nick name" />
+                            </div>
+                            <div className="manage-main___slide-list--content___item--name">
+                                Edit Nicknames
+                            </div>
+                
+                        </div>
+                        <div className="manage-main___slide-list--content___item">
+                            Search in Conversation
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 }
