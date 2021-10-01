b074f8903bccf32c4c4aef61e21ca34298b602b8
quachthanhhmd <48387076+quachthanhhmd@users.noreply.github.com>
2021-10-01 17:59:29 +0700

    Add: login page.

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,20 @@
 import './App.scss';
 import Messager from './pages/Messager';
 import ThemeMode from './components/ThemeMode';
+import { Route, Redirect } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
   return (
-   <div className="App">
-     
-      <ThemeMode/>
-      <Messager/>
-      
-   </div>
+    <div className="App">
+
+      <ThemeMode />
+      {/* <Messager/> */}
+
+      {/* <Route path="/" exact component={Messager} /> */}
+      <Login />
+
+    </div>
 
   );
 }
---
+++ client/src/reducers/user.reducer.js
@@ -0,0 +1,8 @@
+
+export function users(state = {}, action) {
+    switch (action.type) {
+        
+        default:
+            return state
+    }
+}
